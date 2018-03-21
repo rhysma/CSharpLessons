df879cf9de23485c43a304b0df44188fbb32b5ba
Tiffany Ford <rhysma@gmail.com>
2018-03-21 09:14:37 -0500

    Updated Mstest
    package to 1.2.0

--- Section6/Properties/AssemblyInfo.cs
+++ Section6/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Section7")]
+[assembly: AssemblyTitle("Section6")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Section7")]
+[assembly: AssemblyProduct("Section6")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("6b043a79-78b1-4698-96a4-982dd0f621fe")]
+[assembly: Guid("e58149bd-e904-4d76-b29a-d66e485d9c5d")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
