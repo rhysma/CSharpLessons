2d26af857c6b3e2a09b407fbe9bd942fcb764362
Tiffany Ford <rhysma@gmail.com>
2018-03-26 13:31:29 -0500

    Section 9 Complete

---
+++ Section9/MathFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section9
+{
+    class MathFunctions
+    {
+        public static double SquareRoot(double number)
+        {
+            return Math.Sqrt(number);
+        }
+
+        public static int Sum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public static double Sum(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public static decimal Sum(decimal number1, decimal number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
---
+++ Section9/MathHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section9
+{
+    class MathHelper
+    {
+        public static int Square(int aValue)
+        {
+            return aValue * aValue;
+        }
+
+        public static int AddNumber(int number1 = 5, int number2 = 10)
+        {
+            return number1 + number2;
+        }
+
+        public static void NumbersOut(out int value)
+        {
+            value = 9;
+        }
+
+        public static void NumbersRef(ref int value)
+        {
+            value = 7;
+        }
+    }
+}
