27635e4eef4a0d1369f0bc01729b93defc901cf8
nadvolod <nadvolod@gmail.com>
2018-07-01 09:09:37 -0400

    added new projects

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userFirstname;
+            string age;
+
+            //ask the user their name
+            Console.WriteLine("What is your first name?");
+            userFirstname = Console.ReadLine();
+
+            //ask the user their age
+            Console.WriteLine("How old are you?");
+            age = Console.ReadLine();
+
+            string response = "Your name is: " + userFirstname + 
+                              " and you are " + age + " years old";
+            Console.WriteLine(response);
+        }
+    }
+}
